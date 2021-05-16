03209523a098934e0f0607d2d7a30838cc775159
emineakbulut <ezz.akbulut@gmail.com>
2021-05-16 15:00:37 -0700

    Additions

--- utils/CommonMethods.java
+++ utils/CommonMethods.java
@@ -1,6 +1,7 @@
 package utils;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterMethod;
@@ -11,6 +12,7 @@ import java.util.concurrent.TimeUnit;
 public class CommonMethods {
 
     public static WebDriver driver;
+
     @BeforeMethod(alwaysRun = true)
     public static void setup(){
         ConfigsReader.readProperties("C:\\Users\\Work\\eclipse-workspace\\TestNG\\src\\configs\\configuration.properties");
@@ -37,5 +39,9 @@ public class CommonMethods {
         if (driver!=null){
             driver.quit();
         }
+    }
+    public static void sendText(WebElement element,String textToSend){
+        element.clear();
+        element.sendKeys(textToSend);
     }
 }
